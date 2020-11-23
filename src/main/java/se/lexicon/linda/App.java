15de7bb48f494c2dc2434b498305adf65de24a42
Linda Y <linda_saadaldeen@yahoo.com>
2020-11-23 14:25:28 +0100

    Calculater_1st_trying

--- src/main/java/se/lexicon/linda/App.java
+++ src/main/java/se/lexicon/linda/App.java
@@ -1,49 +1,65 @@
 package se.lexicon.linda;
+
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
      Scanner scanner = new Scanner(System.in);
-     String input = "";
-
-        System.out.println( "inter first number" );
-
-        int num1 = scanner.nextInt();
-
-        System.out.println(num1);
-
-        System.out.println( "inter second number" );
-
-        int num2 = scanner.nextInt();
-
-        System.out.println(num2);
-
-        int result = scanner.nextInt();
 
 
-        char operator = '+';
-        switch (operator){
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                result = num1 / num2;
-                break;
-            System.out.print("Result ="+result);
+     char operator;
 
-        }
+     boolean itIsRihgt =true;
+
+
+     while (itIsRihgt) {
+
+         System.out.println("inter first number");
+
+
+         int num1 = scanner.nextInt();
+
+         System.out.println("inter second number");
+
+         int num2 = scanner.nextInt();
+
+
+         System.out.println("select the operator");
+
+         operator = scanner.next().charAt(0);
+
+
+         int result = 0;
+
+
+         switch (operator) {
+             case '+':
+                 result = num1 + num2;
+                 break;
+             case '-':
+                 result = num1 - num2;
+                 break;
+             case '*':
+                 result = num1 * num2;
+                 break;
+             case '/':
+                 result = num1 / num2;
+                 break;
+             default:
+                 System.out.println(operator + " The operator is not available");
+         }
+         System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+
+
+         System.out.println("You can do it again");
+     }
+
+
 
     }
 }
+
+
